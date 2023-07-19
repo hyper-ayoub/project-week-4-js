3eb8e2c75d68ebcaf319186df76e1a9bd054994c
hyper-ayoub <133155846+hyper-ayoub@users.noreply.github.com>
2023-07-19 11:57:00 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+var names1 = ["Yakko"];
+var names2 = ["Jhon"];
+var names3 = ["Jen"];
+var names4 = ["Jason"];
+var names5 = ["Paul"];
+var names6 = ["Frank"];
+var names7 = ["Larry"];
+var names8 = ["Paula"];
+var names9 = ["Laura"];
+var names10 = ["Jim"];
+for (var i = 0; i < names1.length; i++) {
+    console.log("Hello" + names1);
+    console.log("GoodBye" + names2);
+    console.log("GoodBye" + names3);
+    console.log("GoodBye" + names4);
+    console.log("Hello" + names5);
+    console.log("Hello" + names6);
+    console.log("Hello" + names7);
+    console.log("Hello" + names8);
+    console.log("Hello" + names9);
+    console.log("GoodBye" + names10);
+
+
+
+
+
+}
+
+
+
+
